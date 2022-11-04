680ddb193de0534066461cebf787f20a7b83ab13
luiz <luiz.contes@hotmail.com>
2022-11-04 23:14:06 +0000

    01. init 3 projects

---
+++ movie-info-services/src/main/java/contes/movieinfoservices/MovieInfoServicesApplication.java
@@ -0,0 +1,13 @@
+package contes.movieinfoservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServicesApplication.class, args);
+	}
+
+}
