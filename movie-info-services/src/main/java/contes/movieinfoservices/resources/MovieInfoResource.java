60e992d34d96aff152fbbd346a0603639605d2e1
luiz <luiz.contes@hotmail.com>
2022-11-05 04:02:48 +0000

    3. Added Movie Catalog API

---
+++ movie-info-services/src/main/java/contes/movieinfoservices/resources/MovieInfoResource.java
@@ -0,0 +1,19 @@
+package contes.movieinfoservices.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import contes.movieinfoservices.models.Movie;
+
+
+@RestController
+@RequestMapping("/movies")
+public class MovieInfoResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+
+        return new Movie(movieId, "Test name!!!");
+    }
+}
