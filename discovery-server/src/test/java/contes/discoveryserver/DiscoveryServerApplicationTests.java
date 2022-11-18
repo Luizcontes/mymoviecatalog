57461156fbf5590eea1c4216183d174001b18d6c
luiz <luiz.contes@hotmail.com>
2022-11-18 22:55:55 +0000

    test: startup test

---
+++ discovery-server/src/test/java/contes/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package contes.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
